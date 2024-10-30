c9a5c06d97156d7c1c004daa1a1f7166aa17180f
Thales Farias <134720323+thalesfreirefarias@users.noreply.github.com>
2024-10-30 15:20:16 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# bank_typescript
-Banck project using typescript from Alura course
+![Typescript: orientação a objetos](https://imgur.com/9399vxd.png)
+
+# Typescript: orientação a objetos
+
+Este projeto faz parte da escola Front-end e tem como objetivo ensinar a transformar trechos de código originalmente cosntruídos em programação funcional para programação orientada a objetos, destacando as diferenças entre os dois paradigmas.
+
+## Funcionalidades do projeto
+
+O projeto contará com o desenvolvimento de:
+
+- Classe conta e armazenador;
+- Atributos e construtor de conta;
+- Modificadores de acesso;
+- Métodos estáticos;
+- Herança de classes;
+- Decorators de validação.
---
+++ src/types/Conta.ts
@@ -0,0 +1,94 @@
+import { Armazenador } from "./Armazenador.js";
+import { GrupoTransacao } from "./GrupoTransacao.js";
+import { TipoTransacao } from "./TipoTransacao.js";
+import { Transacao } from "./Transacao.js";
+
+export class Conta {
+    protected nome: string;
+    protected saldo: number = Armazenador.obter("saldo") || 0;
+    private transacoes: Transacao[] =Armazenador.obter(("transacoes"), (key: string, value: any) => {
+        if (key === "data") {
+            return new Date(value);
+        }
+        return value;
+    }) || [];
+
+    constructor(nome: string) {
+        this.nome = nome;
+    }
+    public getTitular() {
+        return this.nome;
+    }
+
+    getGruposTransacoes(): GrupoTransacao[] {
+        const gruposTransacoes: GrupoTransacao[] = [];
+        const listaTransacoes: Transacao[] = structuredClone(this.transacoes);
+        const transacoesOrdenadas: Transacao[] = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime());
+        let labelAtualGrupoTransacao: string = "";
+
+        for (let transacao of transacoesOrdenadas) {
+            let labelGrupoTransacao: string = transacao.data.toLocaleDateString("pt-br", { month: "long", year: "numeric" });
+            if (labelAtualGrupoTransacao !== labelGrupoTransacao) {
+                labelAtualGrupoTransacao = labelGrupoTransacao;
+                gruposTransacoes.push({
+                    label: labelGrupoTransacao,
+                    transacoes: []
+                });
+            }
+            gruposTransacoes.at(-1).transacoes.push(transacao);
+        }
+
+        return gruposTransacoes;
+    }
+
+    getSaldo() {
+        return this.saldo;
+    }
+    getDataAcesso(): Date {
+        return new Date();
+    }
+    registrarTransacao(novaTransacao: Transacao): void {
+        if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
+            this.depositar(novaTransacao.valor);
+        }
+        else if (novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA || novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
+            this.debitar(novaTransacao.valor);
+            novaTransacao.valor *= -1;
+        }
+        else {
+            throw new Error("Tipo de Transação é inválido!");
+        }
+
+        this.transacoes.push(novaTransacao);
+        console.log(this.getGruposTransacoes());
+        Armazenador.salvar("transacoes", JSON.stringify(this.transacoes));
+    }
+
+    debitar(valor: number): void {
+        if (valor <= 0) {
+            throw new Error("O valor a ser debitado deve ser maior que zero!");
+        }
+        if (valor > this.saldo) {
+            throw new Error("Saldo insuficiente!");
+        }
+
+        this.saldo -= valor;
+        Armazenador.salvar("saldo", this.saldo.toString());
+    }
+
+    depositar(valor: number): void {
+        if (valor <= 0) {
+            throw new Error("O valor a ser depositado deve ser maior que zero!");
+        }
+
+        this.saldo += valor;
+        Armazenador.salvar("saldo", this.saldo.toString());
+    }
+
+}
+
+
+
+const conta = new Conta("Joana da Silva Olveira");
+
+export default conta;
